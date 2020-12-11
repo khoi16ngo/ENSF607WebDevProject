8e7a7798ac66f33f12f2526fd39323e26b464d65
bshin29 <brandenshin1313@gmail.com>
2020-12-11 10:51:42 -0700

    Moved folder

---
+++ Pre-Project/React/fancy-button/src/App.js
@@ -0,0 +1,71 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import {Button, Form} from 'react-bootstrap';
+import React, {useState} from "react";
+import './App.css';
+
+function App() {
+
+  const [counter, setCounter] = useState(0)
+  const [inputValue, setInputValue] = useState("")
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const setCount = (e) => {
+    e.preventDefault();
+    if (isNaN(Number(inputValue))){
+      alert("Try again: Enter a number!")
+      setCounter(0)
+      return;
+    }
+    setCounter(parseInt(inputValue));
+  };
+
+  const handleKeyDown = (e) => {
+    if(e.key === "Enter"){
+      setCount();
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="leftSide">
+        <div className="btn-group">
+          <Button variant="success"
+          onClick={()=>setCounter(counter +1)}
+          >Increment</Button>
+          <Button variant="danger"
+          onClick={()=>setCounter(counter -1)}
+          >Decrement</Button>
+        </div>
+
+        <div className="inputBox">
+          <Form>
+            <Form.Group controlId="textbox">
+              <Form.Control className="textBox" placeholder="Enter number"
+                value={inputValue}
+                onChange={(e)=> setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit"
+            onClick={setCount}
+            >Set Count
+            </Button>
+          </Form>
+        </div>
+      </div>
+
+      <div className="rightSide">
+        <h1>{counter}</h1>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
